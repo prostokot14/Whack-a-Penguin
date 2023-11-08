dbda8988c493a523b5d2fc9ffed19dba2bef81f6
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-11-08 21:17:17 +0100

    Added movement of penguins to the game

--- Project14/GameViewController.swift
+++ Project14/GameViewController.swift
@@ -7,20 +7,15 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
-class GameViewController: UIViewController {
-
+final class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .fill
                 
-                // Present the scene
                 view.presentScene(scene)
             }
             
@@ -32,14 +27,8 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        if UIDevice.current.userInterfaceIdiom == .phone { .allButUpsideDown } else { .all }
     }
 
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+    override var prefersStatusBarHidden: Bool { true }
 }
---
+++ Project14/WhackSlot.swift
@@ -0,0 +1,61 @@
+//
+//  WhackSlotNode.swift
+//  Project14
+//
+//  Created by Антон Кашников on 08/11/2023.
+//
+
+import SpriteKit
+
+final class WhackSlot: SKNode {
+    private var charNode: SKSpriteNode!
+    private var isVisible = false
+    private var isHit = false
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        addChild(SKSpriteNode(imageNamed: "whackHole"))
+        
+        charNode = SKSpriteNode(fileNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        
+        isVisible = true
+        isHit = false
+        
+        // One-third of the time the penguin will be good, the rest of the time it will be bad.
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + hideTime * 3.5) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
+}
